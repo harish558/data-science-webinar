000724d1981316d50bfe42f534e1160c3b418bfc
Praveen Benedict <you@example.com>
2020-06-02 15:28:56 +0530

    Initial commit

---
+++ programming_demos/demo-error.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    printf("New World \n")
+    return 0;
+}
---
+++ programming_demos/demo.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    int a, b;
+    a =10;
+    b=20;
+    printf("%d\n", a+b);
+}
